92f5719751ea39129be242dbcb01615538dfff68
Michael Einsiedler <meinsiedler@users.noreply.github.com>
2016-11-29 18:16:58 +0100

    Added MVVMLight, added simple loading message mechanism, added simple demo button and about dialog

---
+++ NoSqlExplorer.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Messaging;
+using MaterialDesignThemes.Wpf;
+using NoSqlExplorer.WpfClient.Messages;
+
+namespace NoSqlExplorer.WpfClient.ViewModels
+{
+  public class MainWindowViewModel : ViewModelBase
+  {
+    public MainWindowViewModel()
+    {
+      RegisterMessages();
+    }
+
+    private void RegisterMessages()
+    {
+      Messenger.Default.Register<IsLoadingMessage>(this, m =>
+      {
+        IsLoading = m.IsLoading;
+        IsLoadingReason = m.Reason;
+      });
+    }
+
+    private bool _isLoading;
+    public bool IsLoading
+    {
+      get { return _isLoading; }
+      set { Set(ref _isLoading, value); }
+    }
+
+    private string _isLoadingReason;
+    public string IsLoadingReason
+    {
+      get { return _isLoadingReason; }
+      set { Set(ref _isLoadingReason, value); }
+    }
+
+    private ICommand _demoCommand;
+    public ICommand DemoCommand => _demoCommand ?? (_demoCommand = new RelayCommand(Demo));
+
+    private async void Demo()
+    {
+      Messenger.Default.Send(new IsLoadingMessage {IsLoading = true, Reason = "I'm loading ..."});
+      await Task.Delay(2000);
+      Messenger.Default.Send(new IsLoadingMessage {IsLoading = false});
+    }
+  }
+}
