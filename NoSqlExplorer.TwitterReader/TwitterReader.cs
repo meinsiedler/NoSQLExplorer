4afc0ada179553fe86417cadccd8863bf8d66f40
Michael Einsiedler <meinsiedler@users.noreply.github.com>
2016-12-01 21:46:57 +0100

    #8: implemented TwitterReader for reading feeds line by line until reading is stopped manually or there is nothing more to read

---
+++ NoSqlExplorer.TwitterReader/TwitterReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NoSqlExplorer.TwitterReader
+{
+  public class TwitterReader : ITwitterReader
+  {
+    private readonly object _lock = new object();
+    private readonly string _apiUrl;
+    private CancellationTokenSource _cancellationTokenSource;
+
+    public TwitterReader(string apiUrl)
+    {
+      _apiUrl = apiUrl;
+    }
+
+    public void Start()
+    {
+      _cancellationTokenSource = new CancellationTokenSource();
+      var token = _cancellationTokenSource.Token;
+
+      var request = (HttpWebRequest)WebRequest.Create(_apiUrl);
+      var response = (HttpWebResponse)request.GetResponse();
+
+      Task.Factory.StartNew(() =>
+      {
+        lock (_lock)
+        {
+          IsRunning = true;
+        }
+
+        using (var stream = response.GetResponseStream())
+        {
+          using (var streamReader = new StreamReader(stream))
+          {
+            string line;
+            while (!token.IsCancellationRequested && (line = streamReader.ReadLine()) != null)
+            {
+              var closureLine = line;
+              Task.Run(() => OnNewTweet(closureLine)); // call event on different thread so that event subscribers do not block the reading of subsequent lines
+            }
+          }
+        }
+
+        lock (_lock)
+        {
+          IsRunning = false;
+        }
+      }, token);
+    }
+
+    public void Stop()
+    {
+      _cancellationTokenSource.Cancel();
+    }
+
+    public event Action<string> OnNewTweet = delegate { };
+    public bool IsRunning { get; private set; }
+  }
+}
