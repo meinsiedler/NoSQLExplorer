18d7498d7386feabbd8768b236b2032ca5a7ad02
Markus Dietrich <mdi1984@gmail.com>
2016-12-02 15:38:49 +0100

    added classlibrary project with basic docker container operations (start/stop/getlog)

---
+++ NoSqlExplorer.DockerAdapter/DockerContainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace NoSqlExplorer.DockerAdapter
+{
+  public class DockerContainer
+  {
+    private DockerClient client;
+
+    internal DockerContainer(DockerClient client)
+    {
+      this.client = client;
+    }
+
+    public string Id { get; internal set; }
+    public string Name { get; internal set; }
+    public DockerContainerState State { get; internal set; }
+    public string Status { get; internal set; }
+
+    public async Task<bool> Start()
+    {
+      var started = await this.client.Containers.StartContainerAsync(this.Id, new ContainerStartParameters());
+      this.State = started ? DockerContainerState.Started : DockerContainerState.Exited;
+      return started;
+    }
+
+    public async Task<bool> Stop()
+    {
+      return await this.client.Containers.StopContainerAsync(this.Id, new ContainerStopParameters(), System.Threading.CancellationToken.None);
+    }
+
+    public async Task<Stream> GetContainerLogsAsync(System.Threading.CancellationToken ct)
+    {
+      return await this.client.Containers.GetContainerLogsAsync(
+        this.Id,
+        new ContainerLogsParameters()
+        {
+          ShowStderr = true,
+          ShowStdout = true,
+        },
+        ct);
+    }
+  }
+}
---
+++ NoSqlExplorer.DockerAdapter/DockerInstance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.BasicAuth;
+using Docker.DotNet.Models;
+
+namespace NoSqlExplorer.DockerAdapter
+{
+  public class DockerInstance
+  {
+    private DockerClient client;
+
+    public DockerInstance(string host, int port, string userName, string password)
+    {
+      var credentials = new BasicAuthCredentials(userName, password);
+      this.client = new DockerClientConfiguration(new Uri($"http://{host}:{port}"), credentials).CreateClient();
+    }
+
+    public async Task<IEnumerable<DockerContainer>> GetContainers()
+    {
+      var containers = await client.Containers.ListContainersAsync(new ContainersListParameters
+      {
+        All = true
+      });
+
+      return containers.Select(c => new DockerContainer(this.client)
+      {
+        Id = c.ID,
+        Name = c.Names.FirstOrDefault(),
+        State = c.State == "exited" ? DockerContainerState.Exited : DockerContainerState.Started,
+        Status = c.Status
+      });
+    }
+  }
+}
