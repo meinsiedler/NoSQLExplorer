9897979ad1d36fc14841105ff85ac71e4dfb2d7a
Markus Dietrich <mdi1984@gmail.com>
2016-12-18 18:42:44 +0100

    added class library with start/shutdown/getStatus functionality for azure vms (classic)

---
+++ NoSqlExplorer.AzureAdapter/AzureVirtualMachineStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+
+namespace NoSqlExplorer.AzureAdapter
+{
+  public enum AzureVirtualMachineStatus
+  {
+    Running = 0,
+    Suspended = 1,
+    RunningTransitioning = 2,
+    SuspendedTransitioning = 3,
+    Starting = 4,
+    Suspending = 5,
+    Deploying = 6,
+    Deleting = 7
+  }
+}
