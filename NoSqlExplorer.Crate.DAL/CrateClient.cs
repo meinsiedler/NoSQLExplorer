20a285b3a1b597f1182efecc561c032ad7fbed69
Markus Dietrich <mdi1984@gmail.com>
2017-01-04 12:50:48 +0100

    #3 initial project

---
+++ NoSqlExplorer.Crate.DAL/CrateClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSqlExplorer.Crate.DAL
+{
+  public class CrateClient
+  {
+    private string connectionString;
+
+    public CrateClient(string connectionString)
+    {
+      this.connectionString = connectionString;
+    }
+
+    public async Task SubmitRequest(CrateRequest request)
+    {
+      using (var client = new HttpClient())
+      {
+        var result = client.PostAsync(this.GetRequestAddress(), this.ObjectToHttpContent(request))
+      }
+    }
+
+    private string GetRequestAddress()
+    {
+      return $"{connectionString}/_sql?pretty";
+    }
+
+    private HttpContent ObjectToHttpContent(object obj)
+    {
+      var stringPayload = JsonConvert.SerializeObject(obj);
+      return new StringContent(stringPayload, Encoding.UTF8, "application/json");
+    }
+  }
+}
---
+++ NoSqlExplorer.Crate.DAL/CrateRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSqlExplorer.Crate.DAL
+{
+  public class CrateRequest
+  {
+    public CrateRequest(string statement, params object[] args)
+    {
+      this.Statement = statement;
+      this.Args = args;
+    }
+
+    public string Statement { get; private set; }
+    public object[] Args { get; private set; }
+  }
+}
