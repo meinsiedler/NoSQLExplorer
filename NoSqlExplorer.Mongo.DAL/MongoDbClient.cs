d18826c06866178f277ab9f61f655fa4c4141fb2
Markus Dietrich <mdi1984@gmail.com>
2017-01-11 14:11:51 +0100

    #2 mongo.dal project with response objects and async add method

---
+++ NoSqlExplorer.Mongo.DAL/MongoDbClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using NoSqlExplorer.DAL.Common;
+using NoSqlExplorer.Mongo.DAL.Response;
+
+namespace NoSqlExplorer.Mongo.DAL
+{
+  public class MongoDbClient
+  {
+    private MongoClient client;
+    private IMongoDatabase database;
+
+    public MongoDbClient(string hostname, int port, string username, string password, string databaseName)
+    {
+      var connectionString = string.Empty; 
+      this.client = new MongoClient(connectionString);
+      this.database = this.client.GetDatabase(databaseName);
+    }
+
+    public virtual async Task<IMongoResponse<T>> AddAsync<T>(T item)
+    { 
+      try
+      {
+        await this.database.GetCollection<T>(Helper.GetTableName(typeof(T))).InsertOneAsync(item);
+      }
+      catch (Exception ex)
+      {
+        return new MongoResponse<T>(ex);
+      }
+
+      return new MongoResponse<T>(item);
+    }
+
+  }
+}
---
+++ NoSqlExplorer.Mongo.DAL/Response/MongoResponse.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace NoSqlExplorer.Mongo.DAL.Response
+{
+  public class MongoResponse : IMongoResponse
+  {
+    public MongoResponse()
+    {
+    }
+
+    public MongoResponse(DeleteResult deleteResult)
+    {
+      this.Success = deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+      if (deleteResult.DeletedCount == 0)
+      {
+        this.Errors = new List<string>() { "no matching entries found" };
+      }
+    }
+
+    public MongoResponse(Exception exception)
+    {
+      this.Success = false;
+      this.Errors = new List<string>() { exception.Message };
+    }
+
+    public MongoResponse(bool result)
+    {
+      this.Success = result;
+    }
+
+    public MongoResponse(ReplaceOneResult replaceResult)
+    {
+      if (replaceResult.IsAcknowledged)
+      {
+        if (replaceResult.MatchedCount == 0)
+        {
+          this.Success = false;
+          this.Errors = new List<string>() { "no matching entries found" };
+        }
+        else if (replaceResult.ModifiedCount != 1)
+        {
+          this.Success = false;
+          this.Errors = new List<string>() { "entry not changed" };
+        }
+        else
+        {
+          this.Success = true;
+        }
+      }
+      else
+      {
+        this.Success = false;
+        this.Errors = new List<string>() { "not acknowledged" };
+      }
+    }
+
+    public bool Success { get; private set; }
+    public IEnumerable<string> Errors { get; set; }
+  }
+
+  public class MongoResponse<T> : IMongoResponse<T>
+  {
+    public MongoResponse(T result)
+    {
+      if (result != null)
+      {
+        this.Data = result;
+        this.Success = true;
+      }
+      else
+      {
+        this.Success = false;
+        this.Errors = new List<string>() { "no results found" };
+      }
+    }
+
+    public MongoResponse(Exception ex)
+    {
+      this.Success = false;
+      this.Errors = new List<string>() { ex.Message };
+    }
+
+    public T Data { get; private set; }
+    public bool Success { get; }
+    public IEnumerable<string> Errors { get; }
+  }
+}
