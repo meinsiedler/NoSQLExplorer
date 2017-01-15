251d2f9d5f654a51c8ca5c91c11ca6a010a908f4
Christian Siber <siber.christian@gmail.com>
2017-01-15 15:26:24 +0100

    Mongo.DAL project added to solution

--- NoSqlExplorer.Mongo.DAL/MongoDbClient.cs
+++ NoSqlExplorer.Mongo.DAL/MongoDbClient.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using NoSqlExplorer.DAL.Common;
 using NoSqlExplorer.Mongo.DAL.Response;
+using System;
+using System.Threading.Tasks;
 
 namespace NoSqlExplorer.Mongo.DAL
 {
@@ -16,13 +13,13 @@ namespace NoSqlExplorer.Mongo.DAL
 
     public MongoDbClient(string hostname, int port, string username, string password, string databaseName)
     {
-      var connectionString = string.Empty; 
+      var connectionString = string.Empty;
       this.client = new MongoClient(connectionString);
       this.database = this.client.GetDatabase(databaseName);
     }
 
     public virtual async Task<IMongoResponse<T>> AddAsync<T>(T item)
-    { 
+    {
       try
       {
         await this.database.GetCollection<T>(Helper.GetTableName(typeof(T))).InsertOneAsync(item);
