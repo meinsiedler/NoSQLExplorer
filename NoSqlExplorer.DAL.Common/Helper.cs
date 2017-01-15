f76f0dce5db84809854b177d690ab3057b701f04
Christian Siber <siber.christian@gmail.com>
2017-01-15 13:41:27 +0100

    Fixed build
    * rename Util class to Helper
    * added Helper class to csproj

--- NoSqlExplorer.DAL.Common/Helper.cs
+++ NoSqlExplorer.DAL.Common/Helper.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NoSqlExplorer.DAL.Common
 {
-  public static class Util
+  public static class Helper
   {
     public static string GetTableName(Type type)
     {
